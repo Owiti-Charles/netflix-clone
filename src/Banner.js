37fc0b74b26d81b632bfabd9624d9f07b21685f0
Owiti-Charles <mikeycharlesm7@gmail.com>
2020-08-27 20:21:50 +0300

    style banner

--- src/Banner.js
+++ src/Banner.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import axios from "./axios";
 import requests from "./requests";
+import "./Banner.css";
 
 function Banner() {
   const [movie, setMovies] = useState([]);
@@ -19,33 +20,36 @@ function Banner() {
   }, []); // [] makes the rewuest runs once when the banner loads
   console.log(movie);
 
+  function truncate(str, n){
+      return str?.length > n ? str.substr(0, n-1) + "...": str;
+  } // function to trancate a string 
+
   return (
-    <header className="banner"
-    style = {{
+    <header
+      className="banner"
+      style={{
         backgroundSize: "cover",
         backgroundImage: `url("https://image.tmdb.org/t/p/original/${movie?.backdrop_path}")`,
         backgroundPosition: "center center",
-    }}
+      }}
     >
       <div className="banner__content">
         {/* title */}
-        <h1>
-            {movie?.title || movie?.original_name || movie?.name}
+        <h1 className="banner_title">
+          {" "}
+          {movie?.title || movie?.original_name || movie?.name}
         </h1>
-
-        {/* div with 2 buttons play and more info */}
-        <div className = "banner__buttons">
-            <button className="banner__button">Play</button>
-            <button className="banner__button">My List</button>
-        </div>
-
         {/* description */}
 
-        <h1 className="banner__description">
-            {movie?.overview}
-        </h1>
+        <h2 className="banner__description">{truncate(movie?.overview, 160)}</h2>
 
+        {/* div with 2 buttons play and more info */}
+        <div className="banner__buttons">
+          <button className="banner__button">Play</button>
+          <button className="banner__button_list">My List</button>
+        </div>
       </div>
+      <div className="banner__fade"/>
     </header>
   );
 }
